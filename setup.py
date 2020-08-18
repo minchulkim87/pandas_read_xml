cb0affa8c21c54b39a5deaefbf3def03b448be1d
Min Chul Kim <minchulkim87@gmail.com>
2020-08-19 08:40:52 +1000

    changed to using index when concatenating dataframes as not using was causing errors for 'scalars' only

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import setup
 from setuptools import find_packages
 
-version = "0.0.5"
+version = "0.0.6"
 description = "A tool to read XML files as pandas dataframes."
 
 source_root = Path(".")
